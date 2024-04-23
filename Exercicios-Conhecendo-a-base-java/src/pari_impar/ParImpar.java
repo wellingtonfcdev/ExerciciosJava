bebecb31ad97889c967698ffebdabf1d1a1d21e7
wellingtonfcdev <wellingtonfc@hotmail.com>
2024-04-23 13:29:47 -0300

    Numero Par ou Impar

---
+++ Exercicios-Conhecendo-a-base-java/src/pari_impar/ParImpar.java
@@ -0,0 +1,24 @@
+package pari_impar;
+
+import java.util.Scanner;
+
+/*
+ * Leia um número e imprima se ele é par ou ímpar
+ */
+public class ParImpar {
+    public static void main(String[] args) {
+        
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Informe um número");
+
+        int numero = scanner.nextInt();
+            
+        if(numero % 2 == 0)
+            System.out.println("Número Par");
+        else
+            System.out.println("Número Ímpar");
+
+        scanner.close();
+    }
+}
